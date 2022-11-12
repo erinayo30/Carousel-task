762aa347daa5a22d257dd3e986f3781250fe3d2b
Farinmade <58787138+erinayo30@users.noreply.github.com>
2022-11-12 07:34:53 +0100

    Add files via upload
    CAROUSEL Task

---
+++ index.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelector("[data-carousel-button]")
+
+buttons.forEach(button => {
+    addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = { ...slides.children }.IndexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+});
